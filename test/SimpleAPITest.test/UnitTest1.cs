5cb560a27608eb7e6491af4919011aeaa82792e1
Janis Leite Corteletti <janis.corteletti@gmail.com>
2021-02-24 20:07:30 +0100

    Initial Commit

---
+++ test/SimpleAPITest.test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using SimpleAPI;
+using SimpleAPI.Controllers;
+using Xunit;
+
+namespace SimpleAPITest.test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void Test1()
+        {
+            IEnumerable<WeatherForecast> returnValue = controller.Get();
+            Assert.True(returnValue != null);
+
+        }
+    }
+}
